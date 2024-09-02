34420c47c902517b515289f0623a35b728384210
Nishant <mojonly813@gmail.com>
2024-09-02 21:21:22 +0530

    Some Test

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import LoadingSpinner from "./Spinner"; // Ensure this path is correct
 import Navbar from "./Navbar";
 import "./App.css";
+import Player2 from "./Pages/player/Player2";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true); // Set to true to see the spinner
@@ -13,9 +14,10 @@ export default function App() {
 
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar />
       {isLoading ? <LoadingSpinner /> : renderUser()}
-      {errorMessage && <div className="error">{errorMessage}</div>}
+      {errorMessage && <div className="error">{errorMessage}</div>} */}
+      <Player2 />
     </div>
   );
 }
